c3da210937374afdd1216e8b3dfa420f575ac364
hannamitri <hannamitri7@gmail.com>
2023-10-20 13:14:55 +0300

    pls end this

--- pages/react.tsx
+++ pages/react.tsx
@@ -1,52 +1,52 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
-import Sidebar from "../components/Sidebar";
-import VideoSection from "../components/VideoSection";
-import { mapUrlToVideoId } from "../helpers/constants";
-import { auth } from "../public/firebase";
-import Navbar from "../components/Navbar";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { useRouter } from "next/router";
+// import { useState, useEffect } from "react";
+// import Sidebar from "../components/Sidebar";
+// import VideoSection from "../components/VideoSection";
+// import { mapUrlToVideoId } from "../helpers/constants";
+// import { auth } from "../public/firebase";
+// import Navbar from "../components/Navbar";
 
-function React() {
-  const router = useRouter();
-  const routerPath = router.pathname;
-  const value: any = mapUrlToVideoId.get(routerPath);
-  const user = auth.currentUser;
-  const [, setUser] = useState(null);
-  const [isModal, setIsModal] = useState(false);
-  const [isLogin, setIsLogin] = useState(false);
-  const [isSidebar, setIsSidebar] = useState(false);
+// function React() {
+//   const router = useRouter();
+//   const routerPath = router.pathname;
+//   const value: any = mapUrlToVideoId.get(routerPath);
+//   const user = auth.currentUser;
+//   const [, setUser] = useState(null);
+//   const [isModal, setIsModal] = useState(false);
+//   const [isLogin, setIsLogin] = useState(false);
+//   const [isSidebar, setIsSidebar] = useState(false);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user: any) => {
-      if (user) {
-        setUser(user);
-      }
-    });
-  }, []);
+//   useEffect(() => {
+//     onAuthStateChanged(auth, (user: any) => {
+//       if (user) {
+//         setUser(user);
+//       }
+//     });
+//   }, []);
 
-  return (
-    <>
-      <Navbar
-        user={user}
-        setIsLogin={setIsLogin}
-        setIsModal={setIsModal}
-        setIsSidebar={setIsSidebar}
-        isSidebar={isSidebar}
-      />
-      <div style={{ display: "flex" }}>
-        {isSidebar ? <Sidebar user={user} /> : null}
-        <VideoSection
-          user={user}
-          setIsLogin={setIsLogin}
-          setIsModal={setIsModal}
-        />
-      </div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Navbar
+//         user={user}
+//         setIsLogin={setIsLogin}
+//         setIsModal={setIsModal}
+//         setIsSidebar={setIsSidebar}
+//         isSidebar={isSidebar}
+//       />
+//       <div style={{ display: "flex" }}>
+//         {isSidebar ? <Sidebar user={user} /> : null}
+//         <VideoSection
+//           user={user}
+//           setIsLogin={setIsLogin}
+//           setIsModal={setIsModal}
+//         />
+//       </div>
+//     </>
+//   );
+// }
 
-export default React;
+// export default React;
 
 // export function getPathnameFromVideoId(videoId: any) {
 //   for (const [pathname, id] of mapUrlToVideoId.entries()) {
